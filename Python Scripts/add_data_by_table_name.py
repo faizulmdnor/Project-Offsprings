6f46b7689ffcd5fbe7107a576404031893ebce17
Faizul Md Nor <71113657+faizulmdnor@users.noreply.github.com>
2024-12-20 16:04:38 +0800

    Add new school-related datasets and update script input
    Introduce multiple new datasets, including school placements, school info, classes, grades, and academic years. Updated the `add_data_by_table_name.py` script to utilize the `Penempatan_Sekolah.csv` dataset instead of the `Person.csv`. This enhances data handling and ensures compatibility with the new school placement data.

--- Python Scripts/add_data_by_table_name.py
+++ Python Scripts/add_data_by_table_name.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from offsprings_project import SQL_Operation
 
+data = pd.read_csv('../Data/Penempatan_Sekolah.csv')
 
-data = pd.read_csv('../Data/Person.csv')
-
-
-SQL_Operation.insert_data_no_duplicate(table_name='Person', df=data)
+SQL_Operation.insert_data_no_duplicate(table_name='Penempatan_Sekolah', df=data)
